0be5619e4259c2a3fe48e7d9e8404719c9c63252
golngaz <golngaz@laposte.net>
2023-10-27 11:37:46 +0200

    Update youtube-adblock-blocker-blocker.js

--- youtube-adblock-blocker-blocker.js
+++ youtube-adblock-blocker-blocker.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Adblock Blocker Blocker
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  block the youtube adblock blocker
 // @author       golngaz
 // @match        https://www.youtube.com/*
@@ -12,41 +12,38 @@
 (function() {
     'use strict';
 
-const sleep = 4;
+    const sleep = 4;
 
-
-setTimeout(() => {
-    document.querySelector('button[aria-label="Partager"]:not([style*="display: none"])').click();
 
     setTimeout(() => {
-    setTimeout(() => {
+        document.querySelector('button[aria-label="Partager"]:not([style*="display: none"])').click();
+
+        setTimeout(() => {
+            setTimeout(() => {
 		document.querySelector('#close-button > button:nth-child(1)').click();
 		document.querySelector('yt-playability-error-supported-renderers').remove();
 	}, 0);
 
 	let iframeTemplate = `
-<iframe
-	class=""
-	src="https://www.youtube.com/embed/{{token}}?autoplay=1&amp;auto_play=1"
-	style="position: absolute; top: 0px; left: 0px; max-width: 2100px; max-height: 600px;"
-	width="2100"
-	height="600"
-	frameborder="0"
-	scrolling="no"
-	allowfullscreen=""
-	sandbox="allow-forms allow-modals allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-	tc-textcontent="true"
-	data-tc-id="w-0.147780797001408"
-></iframe>
-`;
+            <iframe
+	        class=""
+	        src="https://www.youtube.com/embed/{{token}}?autoplay=1&amp;auto_play=1"
+	        style="position: absolute; top: 0px; left: 0px; max-width: 2100px; max-height: 600px;"
+	        width="2100"
+	        height="600"
+	        frameborder="0"
+	        scrolling="no"
+	        allowfullscreen=""
+	        sandbox="allow-forms allow-modals allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+	        tc-textcontent="true"
+	        data-tc-id="w-0.147780797001408"
+            ></iframe>`;
 
 	const link = document.querySelector('#share-url').value;
 
 	const token = link.split('/').at(-1).split('?').at(0);
 
 	iframeTemplate = iframeTemplate.replace('{{token}}', token);
-
-	console.log(iframeTemplate, document.querySelector('#container'));
 
 	document.querySelector('#player-container.ytd-watch-flexy').innerHTML = iframeTemplate;
         document.querySelector('#full-bleed-container').style.maxHeight = "600px";
